1e1a88635e96d2028b19bac8f80488170bd7715c
Dean Findlay <D3an79@gmail.com>
2024-12-11 22:03:12 +0000

    added gui elements to dialog

--- diversity_calc.py
+++ diversity_calc.py
@@ -25,6 +25,8 @@ from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction
 
+from qgis.core import QgsMapLayerProxyModel, QgsFieldProxyModel
+
 # Initialize Qt resources from file resources.py
 from .resources import *
 # Import the code for the dialog
@@ -188,6 +190,18 @@ class DiversityCalc:
         if self.first_start == True:
             self.first_start = False
             self.dlg = DiversityCalcDialog()
+
+            # filters the layer selection to only polygon and point layers respectively
+            self.dlg.mcbPoly.setFilters(QgsMapLayerProxyModel.PolygonLayer)
+            self.dlg.mcbPoint.setFilters(QgsMapLayerProxyModel.PointLayer)
+            # filters the field layers to only accept string fields
+            self.dlg.fcbCategory.setFilters(QgsFieldProxyModel.String)
+            self.dlg.fcbSpecies.setFilters(QgsFieldProxyModel.String)
+
+            self.dlg.fcbCategory.setLayer(self.dlg.mcbPoly.currentLayer())
+            self.dlg.fcbSpecies.setLayer(self.dlg.mcbPoint.currentLayer())
+
+
 
         # show the dialog
         self.dlg.show()
